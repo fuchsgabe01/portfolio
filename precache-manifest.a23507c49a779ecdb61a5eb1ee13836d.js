3a3c6c8342d41c4fbe23dc58d96293f8e4c799b9
fuchsgabe01 <fuchsgabe@gmail.com>
2022-05-29 15:30:56 -0400

    Updates

---
+++ precache-manifest.a23507c49a779ecdb61a5eb1ee13836d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc9faf75617b286832ac",
+    "url": "/static/js/main.fc9faf75.chunk.js"
+  },
+  {
+    "revision": "4bb05d8fed1e4d6e8559",
+    "url": "/static/js/1.4bb05d8f.chunk.js"
+  },
+  {
+    "revision": "f166f57f27f9ecf774c3",
+    "url": "/static/js/2.f166f57f.chunk.js"
+  },
+  {
+    "revision": "7af4db86e54e523836d2",
+    "url": "/static/js/runtime~main.7af4db86.js"
+  },
+  {
+    "revision": "2599f9f4f7fadf1d92c29f5a150b8dc0",
+    "url": "/static/media/Resume.2599f9f4.pdf"
+  },
+  {
+    "revision": "49b998bd6f9f318834013d34dabf11f5",
+    "url": "/static/media/headshot5.49b998bd.png"
+  },
+  {
+    "revision": "4a00ddb16d6ec5b33a78ace0277b2e3c",
+    "url": "/static/media/marsship4by3.4a00ddb1.png"
+  },
+  {
+    "revision": "f3b831142b99bf5eb5947dd9d891e6f6",
+    "url": "/index.html"
+  }
+];
